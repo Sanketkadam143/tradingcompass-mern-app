28dd5147067329dfee6add57ef916828cb7341a3
sanket <sanketanilkadam@gmail.com>
2022-09-01 20:54:59 +0530

    added paths

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ import cors from "cors"
 import axios from "axios"
 import * as dotenv from 'dotenv'
 dotenv.config()
+import path from 'path';
+import {fileURLToPath} from 'url';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+})
 
 const PORT = process.env.PORT || 5000;
 
