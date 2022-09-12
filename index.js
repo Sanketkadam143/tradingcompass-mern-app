4255ba8ac7a577fab43194010a2609d9bf5ff49a
sanket <sanketanilkadam@gmail.com>
2022-09-12 18:37:36 +0530

    fixed manifest

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const PORT = process.env.PORT || 5000;
+
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
@@ -15,7 +15,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-
+const PORT = process.env.PORT || 5000;
 
 // if (process.env.NODE_ENV == "production") {
 //   app.use(express.static("client/build"));
